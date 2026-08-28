44415be024c632cd39ab83bd10e1138483524e7c
joernwollny <joern.wollny@gmail.com>
2026-08-28 15:35:22 +0200

    Revert "converted to maven?"
    This reverts commit 297580bd052e452ad7a430772f5281e9828a3845.

--- PokemonCrystal/src/main/java/json/JsonLoader.java
+++ PokemonCrystal/src/main/java/json/JsonLoader.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.Map;
 
 import pokemon.Species;
-import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 public class JsonLoader {
@@ -13,17 +12,19 @@ public class JsonLoader {
 	public static Pokedex importPokedex() throws FileNotFoundException {
 		ObjectMapper mapper = new ObjectMapper();
 
-		Map<String, Species> map = mapper.readValue(new File("src/main/java/json/species.json"),
-				new TypeReference<Map<String, Species>>() {}
-		);
+		Pokedex pokedex =
+				mapper.readValue(
+						new File("src/json/species.json"),
+						mapper.getTypeFactory().constructMapType(
+								Map.class, String.class, Species.class
+								)
+						);
 
-		return new Pokedex(map);
+//		for (Species species : pokedex.values()) {
+//			System.out.println(species.base().getPrimary());
+//		}
+		
+		return pokedex;
 
-	}
-
-	public static void main(String[] args) throws FileNotFoundException {
-		for (Species species : importPokedex().pokedex().values()) {
-			System.out.println(species);
-		}
 	}
 }
