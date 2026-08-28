297580bd052e452ad7a430772f5281e9828a3845
joernwollny <joern.wollny@gmail.com>
2026-08-28 15:21:21 +0200

    converted to maven?

--- PokemonCrystal/src/main/java/json/JsonLoader.java
+++ PokemonCrystal/src/main/java/json/JsonLoader.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Map;
 
 import pokemon.Species;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 public class JsonLoader {
@@ -12,19 +13,17 @@ public class JsonLoader {
 	public static Pokedex importPokedex() throws FileNotFoundException {
 		ObjectMapper mapper = new ObjectMapper();
 
-		Pokedex pokedex =
-				mapper.readValue(
-						new File("src/json/species.json"),
-						mapper.getTypeFactory().constructMapType(
-								Map.class, String.class, Species.class
-								)
-						);
+		Map<String, Species> map = mapper.readValue(new File("src/main/java/json/species.json"),
+				new TypeReference<Map<String, Species>>() {}
+		);
 
-//		for (Species species : pokedex.values()) {
-//			System.out.println(species.base().getPrimary());
-//		}
-		
-		return pokedex;
+		return new Pokedex(map);
 
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		for (Species species : importPokedex().pokedex().values()) {
+			System.out.println(species);
+		}
 	}
 }
